3e397681747cb3d031c554c61c6525739e5376da
Vinicius Rocha <viniciusr.rocha@outlook.com>
2023-01-24 01:25:11 -0300

    Criado projeto base

---
+++ src/main/java/com/vinicius/voting/section/api/VotingSectionApiApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.voting.section.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSectionApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingSectionApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vinicius/voting/section/api/VotingSectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.voting.section.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSectionApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
