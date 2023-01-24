ea753c8f9f9ade0107784b5fbe236fa9ee42e45c
Vinicius Rocha <viniciusr.rocha@outlook.com>
2023-01-24 01:34:21 -0300

    feat: Criado request AgendaRequest

---
+++ src/main/java/com/vinicius/voting/section/api/controller/request/AgendaRequest.java
@@ -0,0 +1,17 @@
+package com.vinicius.voting.section.api.controller.request;
+
+import jakarta.validation.constraints.Size;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public record AgendaRequest(
+        @Size(min = 3, max = 255, message = "Nome nao pode ser menor do que 3 e maior do que 255 caracteres")
+        String name
+) {
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.JSON_STYLE)
+                .append("name", name)
+                .toString();
+    }
+}
